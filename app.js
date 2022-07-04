66ee2a558fe56d973a258c125c8c63d15f0c46f6
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-05 00:03:37 +0900

    4.1 Form Submission
    html form 의 특성을 이용하여 validation 구현
    button -> form submit 으로 하면서 click 에 대해서 신경쓸 필요가 없음

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.log("hello", loginInput.value);
+    const username = loginInput.value;
+    // Validation
+    // if (username === "")
+    // {
+    //     alert("Please write your name");
+    // } else if (username.length > 15) {
+    //     alert("Your name is too long.");
+    // }
+    console.log(username);
 }
+
 loginButton.addEventListener("click", onLoginBtnClick);
