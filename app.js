389dfb5d8d793468760aa7e389f0b63035309437
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-05 00:32:31 +0900

    4.3 Events part two
    피일 확장자 수정 cs -> css
    a tag 동작에 대해서 알아본다

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();     // 브라우저의 기본 동작을 막는다
     // const username = loginInput.value;
@@ -8,4 +10,10 @@ function onLoginSubmit(event) {
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
