7754bee5738091062aedb0bb0132acca0d9b4c05
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-05 00:16:26 +0900

    4.2 Events
    Event Handler 로 등록되는 함수들은 javascript 에서 이벤트에 대한 기본 정보를 넘겨 받을 수 있다

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-// const loginForm = document.getElementById("login-form");
-// // document.querySelector("#login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    // Validation
-    // if (username === "")
-    // {
-    //     alert("Please write your name");
-    // } else if (username.length > 15) {
-    //     alert("Your name is too long.");
-    // }
-    console.log(username);
+function onLoginSubmit(event) {
+    event.preventDefault();     // 브라우저의 기본 동작을 막는다
+    // const username = loginInput.value;
+    // console.log(username);
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
