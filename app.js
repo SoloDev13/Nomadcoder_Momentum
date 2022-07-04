f074a1742061c2900f9093dbca9bc8e00feb3c3d
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-05 00:45:40 +0900

    4.4 Getting Username
    Input 숨김
    Username 가져오기
    h1 숨김 -> 보임, username 표시

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();     // 브라우저의 기본 동작을 막는다
-    // const username = loginInput.value;
-    // console.log(username);
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
