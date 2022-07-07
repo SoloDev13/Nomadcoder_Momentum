8de6344788d5daec558c0962c12a7e5548b77b25
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-07 21:27:01 +0900

    7.1 Adding ToDos
    입력 받은 text 로 li 생성
    li 요소를 ul tag 에 추가

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,21 @@ const toDoInput = document.querySelector("#todo-form input");
 // const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;    // copy
     
     toDoInput.value = "";
+    
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
