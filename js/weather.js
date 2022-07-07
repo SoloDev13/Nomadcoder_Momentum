12333059d2228b60921eaa42ca76089d6bf19284
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-07-08 00:02:12 +0900

    8.1 Weather API
    API Key 를 얻어
    현재 경도, 위도로 날씨 정보를 얻어 온다
    api url 을 실행하는 방법

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,19 @@
 // user's geolocation
 
+const API_KEY = "f157342463aed6406e34f855dd624ab4";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name; 
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });     // promise
 }
 
 function onGeoError() {
