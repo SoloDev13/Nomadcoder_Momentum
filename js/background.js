51f001cedb2646b4118082ce76072acbf70ccd26
SoloDev13 <51294201+SoloDev13@users.noreply.github.com>
2022-08-12 09:20:43 +0900

    css 설정

--- js/background.js
+++ js/background.js
@@ -7,9 +7,13 @@ const images = [
 const choosenImage = images[Math.floor(Math.random() * images.length)];
 
 // element 생성
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${choosenImage}`;
+// bgImage.id = "wallpaper";
+// bgImage.src = `img/${choosenImage}`;
+// bgImage.style = "max-width: 100%; height: auto;";
 
 // element 추가
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style = `background-image: url("img/${choosenImage}"); background-size: 100%, auto; background-repeat: no-repeat;`;
